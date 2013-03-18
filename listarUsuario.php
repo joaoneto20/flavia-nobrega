aab899b4d6afd3f6e9b092460f6f78f492d514d0
joaoneto20 <joaoneto20@gmail.com>
2013-03-18 19:50:19 -0300

    Excluir usuário

--- listarUsuario.php
+++ listarUsuario.php
@@ -1,6 +1,7 @@
 <?php
 require 'headerAdm.php';
 require 'menuAdm.php';
+require 'conexao.php';
 ?>
 <hr>
 <div class="container-fluid">
@@ -11,11 +12,8 @@ require 'menuAdm.php';
             <table class="table table-hover" style="max-width: 800px; margin-left: 20%;">
                 <tbody>
 
-                    <?php
-                    require 'conexao.php';
-
+                    <?php                    
                     $query = "SELECT id, nome, nomeUsuario, email FROM pessoa WHERE id != '' ORDER BY id DESC";
-
                     $exequery = mysql_query($query) or die(mysql_error());
 
                     if (mysql_num_rows($exequery) <= 0) {
@@ -30,7 +28,7 @@ require 'menuAdm.php';
                             echo '<td>' . $res['nomeUsuario'] . '</td>';
                             echo '<td>' . $res['email'] . '</td>';
                             echo '<td><a class ="btn btn-success" href="editarUsuario.php?id=' . $res['id'] . '">Editar</a></td>';
-                            echo '<td><a class ="btn btn-danger" href="#">Excluir</a><br></td>';
+                            echo '<td><a class ="btn btn-danger" href="listarUsuario.php?del=' . $res['id'] . '">Excluir</a><br></td>';
                             echo '</tr>';
                         }
                     }
@@ -41,4 +39,17 @@ require 'menuAdm.php';
 </div>
 <?php
 require 'footerAdm.php';
+?>
+
+<?php
+//###### excluir do banco ########
+
+if(!empty($_GET['del'])){
+    $delid = mysql_real_escape_string($_GET['del']);//pega o id do usuário a ser excluido
+    $querydel = "DELETE FROM pessoa WHERE id = '$delid'";// query para eliminar do banco
+    $exeqrrdel = mysql_query($querydel) or die;//executa a query
+    if($exeqrrdel){
+        echo '<script>alert("Usuário excluído com sucesso!");</script>';
+    }
+}
 ?>
