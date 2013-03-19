2b5c2830fcb23a3701fbfffebdccb7dba8dd68e0
vinicius <vinicius.eneas@gmail.com>
2013-03-19 11:51:03 -0300

    Atualizando IndexUsuario

--- indexUsuario.php
+++ indexUsuario.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Artista Plastico</title>
-        <link type="text/css" rel="stylesheet" href="CSS/styleIndex.css">
-        <script type="text/javascript" src="js/modal.js"></script>
-    </head>
-    <body>
-       <?php include 'confirmacaoUsuario.php';
-        //include 'header.php'; ?>
-
-<h1>Página do Usuário</h1>
-<p>Olá, <?php include 'mostraLogado.php';?>!</p>
-<br>
-<p>você é usuário deste site!</p>
-
-    </body>
-</html>
-
-
+<?php
+require 'headerUsuario.php';
+require 'menuUsuario.php';
+require 'conexao.php';
+?>
+<hr>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span9" style="margin-left: 30%; object-position: center;">
+            <h2>Bem vindo!</h2>
+            <image src="images/gallery_images/logoPainel.png">
+        </div>
+    </div>
+</div>
+<?php
+require 'footerUsuario.php';
+?>
