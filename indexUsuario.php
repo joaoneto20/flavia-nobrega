c815db1273be47d7b48f35ed43bf318aa65acd24
joaoneto20 <joaoneto20@gmail.com>
2013-03-25 21:43:45 -0300

    Ajustes finais alterar senha

--- indexUsuario.php
+++ indexUsuario.php
@@ -1,41 +1,4 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Artista Plastico</title>
-        <link type="text/css" rel="stylesheet" href="CSS/styleIndex.css">
-        <script type="text/javascript" src="js/modal.js"></script>
-    </head>
-    <body>
-       <?php include 'confirmacaoUsuario.php';
-        //include 'header.php';
-       include 'conexao.php'?>
 
-<h1>Página do Usuário</h1>
-<p>Olá, <?php include 'mostraLogado.php';?>!</p>
-<br>
-<p>você é usuário deste site!</p>
-<?php               
-         
-                   
-                    $query = "SELECT * FROM pessoa WHERE nomeUsuario = '".$_SESSION['nomeUsuario']."'";
-                    $exequery = mysql_query($query) or die(mysql_error());
-
-                 $res = mysql_fetch_assoc($exequery);
-                            
-                
-                            echo '<td><a href="alterarSenha.php?id=' . $res['id'] . '">Alterar Senha</a></td>';
-                            
-                 
-                        
-                
-                    ?>
-    </body>
-</html>
-
-
-=======
 <?php
 require 'headerUsuario.php';
 require 'menuUsuario.php';
@@ -53,4 +16,4 @@ require 'conexao.php';
 <?php
 require 'footerUsuario.php';
 ?>
->>>>>>> 5a0f2ac55f4bb02e98e845b5bf5d116518502d34
+
--- menuUsuario.php
+++ menuUsuario.php
@@ -1,54 +1,4 @@
-<<<<<<< HEAD
-<div class="navbar">
-    <div class="navbar-inner">
-        <div class="container-fluid">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="#">Painel Administrativo</a>
-            <div class="nav-collapse collapse" style="height: 0px;">
-                <p class="navbar-text pull-right">
-                    Bem vindo, <?php include 'mostraLogado.php'; ?>!
-                </p>
-                <nav>
-                    <ul class="nav">
-                        <li class="divider-vertical"></li>
-                        <li><a href="indexUsuario.php"><i class="icon-home"></i> Início</a></li>
-                        <li class="divider-vertical"></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Vídeo <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="listarVideoUsuario.php"><i class="icon-th-list"></i> Lista de Vídeo</a></li>
-                            </ul>
-                        </li>
-                        <li class="divider-vertical"></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuário <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
 
-                            <?php               
-         
-                   
-                                $query = "SELECT * FROM pessoa WHERE nomeUsuario = '".$_SESSION['nomeUsuario']."'";
-                                $exequery = mysql_query($query) or die(mysql_error());
-                                $res = mysql_fetch_assoc($exequery);
-                                
-                                    echo'<li><a href="alterarSenha.php?id=' . $res['id'] . '"><i class="icon-th-list"></i> Alterar Senha</a></li>';
-                   
-                                ?>
-                            </ul>
-                        </li>
-                        <li class="divider-vertical"></li>
-                        <li><a href="logout.php"><i class="icon-off"></i> Sair</a></li>
-                        <li class="divider-vertical"></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </div>
-=======
 <div class="navbar">
     <div class="navbar-inner">
         <div class="container-fluid">
@@ -98,5 +48,5 @@
             </div>
         </div>
     </div>
->>>>>>> 5a0f2ac55f4bb02e98e845b5bf5d116518502d34
+
 </div>
